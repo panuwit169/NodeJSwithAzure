6c536edda87d532b7fc9e44647d25958f10ce969
Panuwit Sattayawatanagul <pra169@hotmail.com>
2018-01-19 12:28:15 +0700

    fix error text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("My name is Panuwit Sattayawatanagul/nSend homework to Teacher/nHello, Node JS And Azure");
+    response.end("My name is Panuwit Sattayawatanagul\nSend homework to Teacher\nHello, Node JS And Azure");
 
 });
 
